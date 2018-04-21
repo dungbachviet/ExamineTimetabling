5aaa2f58823c143ff440bd4dd464b85a0adaa9ac
Dung Bach Viet <dungbachviet@gmail.com>
2018-04-22 01:58:23 +0700

    update examineTimetablingProblem.java

--- src/model/ExamineTimetablingProblem.java
+++ src/model/ExamineTimetablingProblem.java
@@ -7,6 +7,14 @@ package model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
+import localsearch.constraints.basic.AND;
+import localsearch.constraints.basic.IsEqual;
+import localsearch.constraints.basic.LessOrEqual;
+import localsearch.functions.element.Element;
+import localsearch.functions.element.ElementTmp;
+import localsearch.functions.max_min.Max;
+import localsearch.functions.max_min.Min;
 import localsearch.model.ConstraintSystem;
 import localsearch.model.LocalSearchManager;
 import localsearch.model.VarIntLS;
@@ -16,54 +24,93 @@ import localsearch.model.VarIntLS;
  * @author DungBachViet
  */
 public class ExamineTimetablingProblem {
-    
+
     public HashMap<String, Area> hmIDToArea;
     public HashMap<String, Course> hmIDToCourse;
     public HashMap<String, ExamClass> hmIDToExamClass;
     public HashMap<String, Room> hmIDToRoom;
     public HashMap<String, Teacher> hmIDToTeacher;
-   
+
     public HashMap<Integer, ExamClass> hmIntegerToExamClass;
-    
+
     public ArrayList<Integer> availableDayList;            // ds các ngày có thể tổ chức thi
     public ArrayList<Integer> jamLevelList;                // ds mức độ tắc nghẽn của các kíp thi
-    
-    
+
     public int[][] commonStudents;
+    public int[] roomSlots;
     public int numExamClass;
     public int numTeacher;
     public int numCourse;
     public int numRoom;
     public int numArea;
-    
-    Area[] areas;
-    Course[] courses;
-    ExamClass[] examClasses;
-    Room[] rooms;
-    Teacher[] teachers;
-    
-    
+
+    ArrayList<Area> areas;
+    ArrayList<Course> courses;
+    ArrayList<ExamClass> examClasses;
+    ArrayList<Room> rooms;
+    ArrayList<Teacher> teachers;
+    Set<Integer> setAvailableDay;
+    ArrayList<ArrayList<Integer>> sameCourseCodeClass;
+
     LocalSearchManager ls;
     ConstraintSystem S;
-    
+
     VarIntLS[] examDays;
     VarIntLS[] examTimeSlots;
     VarIntLS[] examRooms;
     VarIntLS[][] examClassToTeacher;
-    
+
     public void stateModel() {
-        
+
+        // Set up the value domains for variables 
         for (int index = 0; index < numExamClass; index++) {
-            examDays[index] = new VarIntLS(ls, )
+            examDays[index] = new VarIntLS(ls, setAvailableDay);
+            examTimeSlots[index] = new VarIntLS(ls, 0, 3);
+            examRooms[index] = new VarIntLS(ls, 0, numExamClass - 1);
         }
+
+        for (int examClassIndex = 0; examClassIndex < numExamClass; examClassIndex++) {
+            for (int teacherIndex = 0; teacherIndex < 2; teacherIndex++) {
+                examClassToTeacher[examClassIndex][teacherIndex] = new VarIntLS(ls, 0, numTeacher);
+            }
+        }
+
+        // Room Capacity Constraint
+        int numStudentPerClass;
+        for (int examClassIndex = 0; examClassIndex < numExamClass; examClassIndex++) {
+            numStudentPerClass = examClasses.get(examClassIndex).getEnrollmentList().size();
+            S.post(new LessOrEqual(numStudentPerClass, new ElementTmp(roomSlots, examRooms[examClassIndex])));
+        }
+
+        // Constraint - same day and same time-slot if exam classes have the same course code
+        int sameCourseCodeClasslength = sameCourseCodeClass.size();
+        for (int courseIndex = 0; courseIndex < sameCourseCodeClasslength; courseIndex++) {
+            ArrayList<Integer> listClasses = sameCourseCodeClass.get(courseIndex);
+            int length = listClasses.size();
+            VarIntLS[] listTimeSlots = new VarIntLS[length];
+            VarIntLS[] listDays = new VarIntLS[length];
+            for (int index = 0; index < length; index++) {
+                listTimeSlots[index] = examTimeSlots[listClasses.get(index)];
+                listDays[index] = examDays[listClasses.get(index)];
+            }
+
+            S.post(new AND(
+                    new IsEqual(new Max(listTimeSlots), new Min(listTimeSlots)),
+                    new IsEqual(new Max(listDays), new Min(listDays))));
+        }
+        
+        
+        // Constraint : not allowed to place an exam class in a room's any time unit of busy time list
+        for (int examClassIndex = 0; examClassIndex < numExamClass; examClassIndex++) {
+            
+        }
+        
+        
+
     }
-    
-    
-    
-    
-    
+
     public static void main(String[] args) {
         System.out.println("");
     }
-      
+
 }
